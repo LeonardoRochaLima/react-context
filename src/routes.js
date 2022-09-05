68cf7bc44e4e14fbfc47b269ed7c06dbd0c3fb6b
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-05 15:04:30 -0300

    Passando os estados de Usuário para a Feira

--- src/routes.js
+++ src/routes.js
@@ -8,14 +8,14 @@ function Router() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/">
-          <UsuarioProvider>
+        <UsuarioProvider>
+          <Route exact path="/">
             <Login />
-          </UsuarioProvider>
-        </Route>
-        <Route path="/feira">
-          <Feira />
-        </Route>
+          </Route>
+          <Route path="/feira">
+            <Feira />
+          </Route>
+        </UsuarioProvider>
         <Route path="/carrinho">
           <Carrinho />
         </Route>
