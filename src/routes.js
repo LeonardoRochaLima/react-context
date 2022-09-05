9e006b1210a74524047d9bf952828345182f7a6c
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-05 14:19:17 -0300

    Criando o Provider no Contexto para compartilhar

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,17 @@
 import Carrinho from "pages/Carrinho";
 import Feira from "pages/Feira";
 import Login from "pages/Login";
-import { useState } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { UsuarioContext } from 'common/contexts/Usuario'
+import UsuarioProvider from 'common/contexts/Usuario'
 
 function Router() {
-  const [nome, setNome] = useState('');
-  const [saldo, setSaldo] = useState(0);
-
   return (
     <BrowserRouter>
       <Switch>
         <Route exact path="/">
-          <UsuarioContext.Provider value={{ nome, setNome, saldo, setSaldo }}>
+          <UsuarioProvider>
             <Login />
-          </UsuarioContext.Provider>
+          </UsuarioProvider>
         </Route>
         <Route path="/feira">
           <Feira />
