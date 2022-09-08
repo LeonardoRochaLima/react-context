4f223bac66286f731ed2d238fc1807239286e8ad
@leonardo.lima <leonardo.lima@altatech.com.br>
2022-09-08 11:08:15 -0300

    Criando função de goBack para voltar da tela de carrinho para feira

--- src/pages/Carrinho/index.js
+++ src/pages/Carrinho/index.js
@@ -3,14 +3,16 @@ import MuiAlert from '@material-ui/lab/Alert';
 import { useCarrinhoContext } from 'common/contexts/Carrinho';
 import Produto from 'components/Produto';
 import { useState } from 'react';
+import { useHistory } from 'react-router-dom';
 import { Container, Voltar, TotalContainer, PagamentoContainer } from './styles';
 
 function Carrinho() {
   const [openSnackbar, setOpenSnackbar] = useState(false);
   const { carrinho } = useCarrinhoContext();
+  const history = useHistory()
   return (
     <Container>
-      <Voltar />
+      <Voltar onClick={() => history.goBack()} />
       <h2>
         Carrinho
       </h2>
